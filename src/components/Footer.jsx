6ab20242f4c9d01fa07740161e8fb40f05bb805b
Hoang Phuc <nvhphuc364@gmail.com>
2024-09-19 00:23:33 +0700

    Setup project, create react components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    const curYear = new Date().getFullYear()
+    return <footer>
+        <p>
+            Copyrightⓒ {curYear}
+        </p>
+    </footer>
+}
+
+export default Footer
