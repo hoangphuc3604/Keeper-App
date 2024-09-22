48eeb37e5abb5721243b1e872e7f63d3aaaaf39c
Hoang Phuc <nvhphuc364@gmail.com>
2024-09-22 23:32:44 +0700

    Add new note and delete note case

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
 import NewNote from "./NewNote"
-import notes from "../notes"
+import oNotes from "../notes"
 
 function App() {
+    const [notes, updateNotes] = useState(oNotes)
+
+    function handleAdd(note) {
+        updateNotes(prevNotes => [...prevNotes, {
+            ...note,
+            key: notes[notes.length - 1].key + 1
+        }])
+    }
+
+    function handleDelete(title) {
+        updateNotes(prevNotes => {    
+            const noteIndex = prevNotes.findIndex(note => note.title === title)
+                  
+            if (noteIndex) {
+                prevNotes.splice(noteIndex, 1)
+            }
+
+            return prevNotes
+        })
+    }
+
     return <div>
         <Header />
 
-        <NewNote />
+        <NewNote 
+            onAdd={handleAdd}
+        />
         
-        {notes.map((note) => <Note 
-            key={note.key}
+        {notes.map((note, index) => <Note 
+            id={index}
+            key={index}
             title={note.title}
             content={note.content}
+            onDelete={handleDelete}
         />)}
 
         <Footer />
